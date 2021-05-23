d03a73333abe1137a7d07404e414f4543a1d520f
Tatiana Joseph <tjoseph0696@gmail.com>
2021-05-23 15:52:00 -0400

    Bugs fixed

--- js/myscript.js
+++ js/myscript.js
@@ -1,31 +1,28 @@
 function computerPlay(){
-	return Math.floor(Math.random()*3)===0? "Rock": 
-		Math.floor(Math.random()*3)===1? "Paper": "Scissors"; 
+	return Math.floor(Math.random()*3)===0? 'Rock': 
+		Math.floor(Math.random()*3)===1? 'Paper': 'Scissors'; 
 }
 
 function playRound(playerSelection, computerPlay){
-	//If player wins return 1
-	//if player loses return -1 
-	//if its a tie return 0
 	playerSelection=playerSelection.toLowerCase();
-	if(playerSelection==="rock" && computerPlayer==="Scissors"){
-		console.log("You Win! Rock beats Scissors.");
+	if(playerSelection==='rock' && computerPlayer==='Scissors'){
+		console.log('You Win! Rock beats Scissors.');
 		return 1;
 	}
-	else if(playerSelection==="scissors" && computerPlayer==="Paper"){
-		console.log("You Win! Scissors beats Paper.");
+	else if(playerSelection==='scissors' && computerPlayer==='Paper'){
+		console.log('You Win! Scissors beats Paper.');
 		return 1;
 	}
-	else if( playerSelection==="paper" && computerPlayer==="Rock"){
-		console.log("You Win! Paper beats Rock.");
+	else if( playerSelection==='paper' && computerPlayer==='Rock'){
+		console.log('You Win! Paper beats Rock.');
 		return 1;
 	}
 	else if(playerSelection===computerPlayer.toLowerCase()){
-		console.log("It's a Tie!");
+		console.log('It\'s a Tie!');
 		return 0;
 	}
 	else{
-		console.log("You Lose:(");
+		console.log('You Lose:(');
 		return -1;
 	}
 }
@@ -33,15 +30,19 @@ function playRound(playerSelection, computerPlay){
 function game(){
 	rounds=5;
 	winner=0;
-	for(let i=0;i<rounds;i++){
-		playerInput=prompt("Enter your move: ");
-		for(playerInput.toLowerCase()!=="rock"|| playerInput.toLowerCase()!=="scissors" playerInput.toLowerCase()!=="paper"){
-			playerInput=prompt("Invalid input! Try again: ");
+	for(let i=0; i<rounds; i++){
+		playerInput=prompt('Enter your move: ');
+		while( playerInput.toLowerCase() != 'rock' &&  playerInput.toLowerCase() != 'scissors' && playerInput.toLowerCase() != 'paper' ){
+			playerInput=prompt('Invalid input! Try again: ');
 		}
-		winner+=playRound(playerInput, computerPlay)
+		winner+=playRound(playerInput, computerPlay);
 
 	}
 	
-	return winner>0? "You won this game!": 
-	      winner==0? "You tied this game!": "You lost this game:(";  
+	return winner>0? 'You won this game!': 
+	      winner==0? 'You tied this game!': 'You lost this game:(';  
 }
+
+//let message=document.createElement("p");
+//message.innerHTML("Testing this document...");
+game();
